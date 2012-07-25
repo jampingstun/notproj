d9abb9803c3f1eaaa0b49c7b45e8ea6e710ebb9d
pradanasetialana <pradana.exe@gmail.com>
2012-07-25 10:39:41 +0700

    cari

--- controller/cari.php
+++ controller/cari.php
@@ -1,13 +1,15 @@
 <?php
-$f = $_POST['f'];
+$f = $_GET['f'];
 $sql = "SELECT * FROM tbl_index WHERE tipe='".$f['tipe']."' and kode='".$f['kode']."' and isi='".$f['isi']."'";
 $data = mysql_query($sql) or die(mysql_error());
 if (mysql_num_rows($data)>0) {
     while ($row = mysql_fetch_array($data)) {
         $id = $row['id'];
+        echo $id;
     }
-    $sql = "SELECT * FROM ".$f['tipe']." WHERE idpemohon='".$id."'";
-    //kode tampil di view
+    $sql = "SELECT * FROM ".$f['tipe']." WHERE id".$f['tipe']."='".$id."'";
+    $result = mysql_query($sql) or die(mysql_error());
+    while ($row = mysql_fetch_array($result)) $row = $data;
 }
 else {
     echo "data tidak ditemukan";
