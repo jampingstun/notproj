cd113c5f3a44c409655837280b20ce68492f4ca7
pradanasetialana <pradana.exe@gmail.com>
2012-07-25 14:02:43 +0700

    komet

--- view/menu.php
+++ view/menu.php
@@ -21,31 +21,27 @@
     </div>
     <div class="row">
         <div class="span3">
-            
+            <form action="index.php" method="get">
                 <ul class=" nav nav-tabs nav-stacked menu">
                     <li ><a style="padding-right: 17px;" class="disabled menuhead"><center><b>MENU</b></center></a></li>
-                    <li><a href="#"><i class="icon-home"></i>&nbsp;&nbsp;<b>Home</b></a></li>
+                    <li><a href="?op=tgrouptransaksi"><i class="icon-home"></i>&nbsp;&nbsp;<b>Home</b></a></li>
                     <li><a href="#" data-toggle="collapse" data-target="#Mpemohon"><i class="icon-user"></i>&nbsp;&nbsp;<b>Data Pemohon</b> </a></li>
                     <div  style="background-color: #f5f5f5;   border: 1px solid #eee; border: 1px solid rgba(0, 0, 0, 0.05);" >
                         <ul id="Mpemohon" class="submenu collapse ">
-                            <li><a href="#"><i class="icon-plus-sign"></i>&nbsp; input pemohon</a></li>
+                            <li><a href="?op=pemohon"><i class="icon-plus-sign"></i>&nbsp; input pemohon</a></li>
                             <li><a href="#"><i class="icon-search"></i> &nbsp;lihat pemohon</a></li>     
                         </ul>
                     </div>
                     <li><a href="#" data-toggle="collapse" data-target="#Mtransaksi"><i class="icon-list-alt"></i>&nbsp;&nbsp;<b>Data Transaksi</b></a></li>
                     <div  style="background-color: #f5f5f5;   border: 1px solid #eee; border: 1px solid rgba(0, 0, 0, 0.05);" >    
                         <ul id="Mtransaksi" class="submenu collapse">
-                            <li ><a href="#"><i class="icon-plus-sign"></i>&nbsp; input transaksi</a></li>
+                            <li ><a href="?op=tgrouptransakso"><i class="icon-plus-sign"></i>&nbsp; input transaksi</a></li>
                             <li ><a href="#"><i class="icon-search"></i> &nbsp;lihat transaksi</a></li>                    
                         </ul>
                     </div>
                     <li><a class="disabled"><br/></a></li>
                 </ul>
+            </form>
         </div>
-        <div class="span9">
-<!--     content goes here       -->
-       <? include 'view/tgrouptransaksi.php';?>
-        </div>
-    </div>
-</div>    
+    
 
